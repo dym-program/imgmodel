cae1a3fdf512c434614d4af1244bd74bd99cbd92
ugpu <yeomanlucky@gmail.com>
2024-04-15 23:24:50 +0800

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
 export const API_BASE_URL = 'http://localhost:3001/api'; // 用于API调用
 export const IMAGE_BASE_URL = 'http://localhost:3001/images';   // 用于图片和其他静态资源
+export const MODEL_BASE_URL = 'http://localhost:3001/models';   // 用于图片和其他静态资源
