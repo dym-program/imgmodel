68ddb0f2f1267553e807074ee851f2d307a4c68b
ugpu <yeomanlucky@gmail.com>
2024-04-15 18:02:19 +0800

    update img

--- client/src/components/ModelViewer/ModelViewer.jsx
+++ client/src/components/ModelViewer/ModelViewer.jsx
@@ -6,27 +6,39 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const ModelViewer = () => {
   const mountRef = useRef(null);
-  const sceneRef = useRef(new THREE.Scene()); // 使用一个共享的场景引用
+  const scene = useRef(new THREE.Scene()).current;
+  const camera = useRef(new THREE.OrthographicCamera(
+    window.innerWidth / -2, 
+    window.innerWidth / 2, 
+    window.innerHeight / 2, 
+    window.innerHeight / -2, 
+    1, 
+    1000
+  )).current;
+  const renderer = useRef(new THREE.WebGLRenderer({ antialias: true, alpha: true })).current;
   const [images, setImages] = useState([]);
-  const cameraRef = useRef(new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000));
-  const rendererRef = useRef(new THREE.WebGLRenderer({ antialias: true, alpha: true }));
+  const imageGroup = useRef(new THREE.Group());
+  scene.add(imageGroup.current);
 
   useEffect(() => {
-    rendererRef.current.setSize(window.innerWidth, window.innerHeight);
-    rendererRef.current.setClearColor(0x000000, 0); // 透明背景
-    mountRef.current.appendChild(rendererRef.current.domElement);
-    new OrbitControls(cameraRef.current, rendererRef.current.domElement);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000, 0);
+    mountRef.current.appendChild(renderer.domElement);
+    new OrbitControls(camera, renderer.domElement);
+    camera.position.z = 500;
 
-    cameraRef.current.position.z = 500; // 调整相机位置
+    scene.add(new THREE.AmbientLight(0x404040));
 
-    const ambientLight = new THREE.AmbientLight(0x404040); // 添加环境光
-    sceneRef.current.add(ambientLight);
+    const resizeListener = () => {
+      camera.left = window.innerWidth / -2;
+      camera.right = window.innerWidth / 2;
+      camera.top = window.innerHeight / 2;
+      camera.bottom = window.innerHeight / -2;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
 
-    const spotLight = new THREE.SpotLight(0x9932CC, 1.5, 1000, Math.PI / 4, 0.5, 2);
-    spotLight.position.set(0, 0, 100);
-    spotLight.target.position.set(0, 100, 0);
-    sceneRef.current.add(spotLight);
-    sceneRef.current.add(spotLight.target);
+    window.addEventListener('resize', resizeListener);
 
     const loadImages = async () => {
       try {
@@ -40,45 +52,36 @@ const ModelViewer = () => {
     loadImages();
 
     const animate = () => {
-      sceneRef.current.rotation.y += 0.01; // 添加场景旋转
-      rendererRef.current.render(sceneRef.current, cameraRef.current);
       requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+      imageGroup.current.rotation.y += 0.005;
     };
 
     animate();
 
-    window.addEventListener('resize', () => {
-      cameraRef.current.aspect = window.innerWidth / window.innerHeight;
-      cameraRef.current.updateProjectionMatrix();
-      rendererRef.current.setSize(window.innerWidth, window.innerHeight);
-    });
-
     return () => {
-      mountRef.current.removeChild(rendererRef.current.domElement);
+      window.removeEventListener('resize', resizeListener);
+      mountRef.current.removeChild(renderer.domElement);
     };
   }, []);
 
   useEffect(() => {
     const textureLoader = new THREE.TextureLoader();
-    const radius = 150; // 螺旋的半径
-    let currentHeight = -300; // 初始化高度
+    const radius = 300;
+    let currentHeight = -300;
 
-    images.forEach((imgSrc, index) => {
+    images.slice(0, 10).forEach((imgSrc, index) => {
       textureLoader.load(`${IMAGE_BASE_URL}/${imgSrc}`, texture => {
         const aspectRatio = texture.image.height / texture.image.width;
-        const geometry = new THREE.PlaneGeometry(100 * aspectRatio, 100); // 调整图片的尺寸
+        const geometry = new THREE.PlaneGeometry(100 * aspectRatio, 100);
         const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true, side: THREE.DoubleSide });
         const mesh = new THREE.Mesh(geometry, material);
 
-        const angle = index * 0.5; // 增加旋转间隔
-        mesh.position.x = radius * Math.cos(angle);
-        mesh.position.y = currentHeight; // 增加垂直间距
-        mesh.position.z = radius * Math.sin(angle);
-        currentHeight += 90; // 增加间距值
+        const angle = index * 0.5;
+        mesh.position.set(radius * Math.cos(angle), currentHeight, radius * Math.sin(angle));
+        currentHeight += 90;
 
-        mesh.lookAt(cameraRef.current.position); // 使图片始终面向相机
-
-        sceneRef.current.add(mesh);
+        imageGroup.current.add(mesh);
       });
     });
   }, [images]);
